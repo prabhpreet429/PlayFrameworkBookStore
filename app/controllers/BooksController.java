2b333b60510fdeb7291498a5c023b93f8312a62c
Prabhpreet Singh <prabhpreet429@gmail.com>
2020-03-22 01:51:47 -0400

    adding book controllers

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,45 @@
+package controllers;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+
+public class BooksController extends Controller {
+
+    //FOR ALL BOOKS
+    public Result index() {
+    return TODO;
+    }
+
+    //TO CREATE BOOK
+    public Result create() {
+
+    }
+
+    //saving
+    public Result save()
+    {
+
+    }
+
+    //edit
+    public Result edit(Integer id) {
+
+    }
+
+    //update
+    public Result update()
+    {
+
+    }
+
+
+    //delete
+    public Result delete(Integer id){
+
+    }
+
+    //book details
+    public  Result show(Integer id) {
+
+    }
+}
